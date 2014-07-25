a4907e2bb29d8cc8e0da8c91b6cbb92c3c01c094
Jake Coxon <jake.coxon@ovoenergy.com>
2014-07-25 15:41:35 +0100

    Convert view to react

--- game.js
+++ game.js
@@ -19,12 +19,12 @@ function set(obj, key, el) {
 }
 
 function getTile(x, y) {
-    return !tiles[y] ? tileBuilder[0]() :
-           !tiles[y][x] ? tileBuilder[0]() : tiles[y][x];
+    return !model.tiles[y] ? tileBuilder[0]() :
+           !model.tiles[y][x] ? tileBuilder[0]() : model.tiles[y][x];
 }
  
 function getBlockedByEntity(x, y) {
-    return _.find(entities, function(entity) {
+    return _.find(model.entities, function(entity) {
         return entity.x == x && entity.y == y;
     });
 }
@@ -42,8 +42,9 @@ function Tile(blockId, tileDef, opts) {
     _.extend(this, tileDef, opts);
 }
 
-function Entity(x, y) {
+function Entity(x, y, className) {
     this.x = x; this.y = y;
+    this.className = className;
     
     this.onMoveCallback = function() {};
     
@@ -89,6 +90,9 @@ var tileBuilder = {};
     immutableTile(1, { solid: true });
     immutableTile(3, { solid: true });
     immutableTile(4, { solid: true });
+    immutableTile(5, { solid: false });
+    immutableTile(6, { solid: false });
+    immutableTile(7, { solid: false });
     
     set(tileBuilder, 2, function() {
         return new Tile(2, null, {
@@ -98,6 +102,22 @@ var tileBuilder = {};
                 if (this.money > 0) {
                     this.money = 0;
                     console.log("Add money!");
+                    this.blockId = "2-open";
+                    return {turns: 1};
+                }
+            }
+        });
+    });
+
+    set(tileBuilder, 8, function() {
+        return new Tile(8, null, {
+            solid: true,
+            open: false,
+            onBump: function() {
+                if (!this.open) {
+                    this.open = true;
+                    this.solid = false;
+                    this.blockId = "8-open";
                     return {turns: 1};
                 }
             }
@@ -106,11 +126,10 @@ var tileBuilder = {};
     
 })();
 
-
 var tiles = build(tileBuilder, function() { /*
 111111111111111111111111111111111111111111
 1         1                   1          1
-1         1                   4  2 2     1
+1         1              5    8  2 2     1
 1         1                   1          1
 1     2   111111  11111111111111111      1
 4                 13333333333            1
@@ -119,23 +138,30 @@ var tiles = build(tileBuilder, function() { /*
 4     33333333  2 1           2 2 2      1
 1                 1                      1
 1                                        1
-1111111             111141111  11111111111
-1111111   1111111               1
-1111111   1111111               1
+1111111             111181111  11111111111
+1111111  61111111              61
+1111111 7 1111111      6        1
 111111111111111111111111111111111
 */});
 
-var boardWidth = _.max(_.map(tiles, function(line) { return line.length; })),
-        boardHeight = tiles.length;
-var entities = [];
-var enemies = [];
+var enemies = [ 
+  new Entity(1, 4, "enemy"), 
+  new Entity(4, 4, "enemy"), 
+  new Entity(5, 4, "enemy") ];
 
-var player = push(entities, new Entity(1, 1));
+var player = new Entity(1, 1, "player");
 
+var model = {
+    tiles : tiles,
 
-push(entities, push(enemies, new Entity(1, 4)));
-push(entities, push(enemies, new Entity(4, 4)));
-push(entities, push(enemies, new Entity(5, 4)));
+    boardWidth: _.max(_.map(tiles, function(line) { return line.length; })),
+    boardHeight: tiles.length,
+
+    entities: [ player ].concat( enemies ),
+    player: player
+
+}
+
 
 function onPlayerBumpTile(tile) {
     if (tile.onBump) {
--- view.js
+++ view.js
@@ -1,65 +1,92 @@
-$(function() {
-  
-var c = $("#container");
-var inner = $("#inner");
 
 var tileWidth = 32, tileHeight = 32;
 
 
-inner.css( { 'width': boardWidth * tileWidth, 'height': boardHeight * tileHeight } );
+var View = React.createClass({
+    render: function() {
+        var tiles = [];
 
-for ( var j = 0; j < boardHeight; j++ ) {
-    for ( var i = 0; i < boardWidth; i++ ) {
-        var d = $("<div>").addClass("tile");
-        d.addClass( "block" + getTile(i, j).blockId );
-        inner.append(d);
+        var boardWidth = this.props.model.boardWidth,
+            boardHeight = this.props.model.boardHeight;
+
+        for ( var j = 0; j < boardHeight; j++ ) {
+
+            for ( var i = 0; i < boardWidth; i++ ) {
+
+                var blockId = getTile(i, j).blockId;
+                var key = j * boardWidth + i;
+
+                tiles.push(
+                    new React.DOM.div({ key: key, className: "tile block" + blockId }, "")
+                );
+
+            }
+        }
+
+        _.each( this.props.model.entities, function( entity ) {
+
+            var style = {
+                left: entity.x * tileWidth,
+                top: entity.y * tileHeight
+            };
+
+            tiles.push(
+                new React.DOM.div({ className: entity.className, style: style })
+            );
+
+        } );
+
+        var style = { width: boardWidth * tileWidth, 
+                      height: boardHeight * tileHeight };
+
+        return new React.DOM.div({ id: "inner", style: style }, tiles);
     }
-}
-
-function attachEntityCallback(entity, div) {
-  
-    entity.onMoveCallback = function() {
-        div.css({ "left": entity.x * tileWidth,
-                  "top": entity.y * tileHeight });
-    };
-    entity.onMoveCallback();
-    return entity.onMoveCallback;
-}
-
-
-
-var playerDiv = $("<div>").addClass("player").appendTo(inner);
-attachEntityCallback(player, playerDiv);
-
-_.each(enemies, function(enemy) {
-    var div = $("<div>").addClass("enemy").appendTo(inner);
-    attachEntityCallback(enemy, div);
 });
 
+function refresh() { 
+    React.renderComponent(new View({ model: model }), document.getElementById('container'));
+}
+
+refresh();
+
+
+
+
+$(function() {
+  
 $(window).on("keydown", function(ev) {
 
     var dx = (ev.keyCode == 39) - (ev.keyCode == 37);
     var dy = (ev.keyCode == 40) - (ev.keyCode == 38);
+    
     if (dx || dy) {
-        player.move(dx, dy);
+        model.player.move(dx, dy);
+        refresh();
         return false;
     }
+
 });
 
-function update() {
-  
-    var targetX = -(player.x * tileWidth - 200),
-        targetY = -(player.y * tileHeight - 200);
-    var currentX = inner.position().left,
-        currentY = inner.position().top;
+$(function() {
+
+
+    var inner = $("#inner");
+
+    function update() {
       
-    inner.css('left', currentX + (targetX - currentX) / 20);
-    inner.css('top', currentY + (targetY - currentY) / 20);
+        var targetX = -(model.player.x * tileWidth - 200),
+            targetY = -(model.player.y * tileHeight - 200);
+        var currentX = inner.position().left,
+            currentY = inner.position().top;
+          
+        inner.css('left', currentX + (targetX - currentX) / 20);
+        inner.css('top', currentY + (targetY - currentY) / 20);
 
-    requestAnimationFrame(update);
-}
+        requestAnimationFrame(update);
+    }
 
-update();
+    update();
+});
 
 });
 
